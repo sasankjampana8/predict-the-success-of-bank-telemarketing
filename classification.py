3e8538577db08f5e4f3b407e498ac73eaa7210fe
sasankjampana8 <sasankjampana8@gmail.com>
2024-10-28 19:18:30 +0530

    codeupdates

--- classification.py
+++ classification.py
@@ -6,6 +6,8 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
 import pickle as pkl
 import os
+from sklearn.preprocessing import MinMaxScaler
+import xgboost as xg
 
 
 def read_data(file_path):
@@ -40,18 +42,53 @@ class DataPreprocessing:
         with open(f"{self.artifacts_path}/{feature_name}_estimator", 'wb') as f:
             pkl.dump(obj, f)
         
-    def label_encoding(self, X_train, X_val):
+    def label_encoding(self, ):
         label_encoder = LabelEncoder()
         cols = []
         cat_cols = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'poutcome', 'target']
 
-    def onehot_encoding(self, X_train, X_val):
+    def onehot_encoding(self, ):
         onehot_encoder = OneHotEncoder()
         cols = []
+        
+    def feature_scaling(self, ):
+        cols_to_scale = ['age', 'balance', 'days_since_last_contact', 'duration', 'campaign', 'pdays', 'previous']
+        pass
+
+    def feature_creation(self, ):
+        today = pd.Timestamp.today().normalize()
+
+        self.X_train['last contact date'] = pd.to_datetime(self.X_train['last contact date'])
+        self.X_train['days_since_last_contact'] = (today - self.X_train['last contact date']).dt.days
+        
+        self.X_val['last contact date'] = pd.to_datetime(self.X_val['last contact date'])
+        self.X_val['days_since_last_contact'] = (today - self.X_val['last contact date']).dt.days
+
+
+        pass
     
-    def procesing(self, X_train, X_val):
+    def procesing(self,):
         pass
         
+
+
+class ModelBuilding:
+    def __init__(self, X_train, X_val, Y_train, Y_val) -> None:
+        self.X_train = X_train
+        self.X_val  = X_val
+        self.Y_train = Y_train
+        self.Y_val =  Y_val
+    
+    
+    
+    def train_model(self,):
+        model = xg.XGBClassifier()
+        model.fit(self.X_train, self.Y_train)
+        
+        
+
+
+
 
 if __name__=='__main__':
     train_path = 'data/train.csv'
@@ -61,7 +98,8 @@ if __name__=='__main__':
     X = data.drop(['target'], axis=1)
     # Data preprocessing
     obj = DataPreprocessing(X, y)
-    visualizations(data)
+    # visualizations(data)
+    X_train_processed, X_val_processed = obj.procesing()
 
     
 
